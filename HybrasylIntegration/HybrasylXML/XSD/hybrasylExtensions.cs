fbb34ffd38cfaf4412b95678577198cfd7156a15
Justin Baugh <baughj@hybrasyl.com>
2016-12-20 12:56:55 -0500

    Add properties with defaults for use in server / etc

--- HybrasylIntegration/HybrasylXML/XSD/hybrasylExtensions.cs
+++ HybrasylIntegration/HybrasylXML/XSD/hybrasylExtensions.cs
@@ -34,14 +34,86 @@ namespace Hybrasyl.Items
         [XmlIgnore]
         public Item ParentItem { get; set; }
 
+        #region Accessors to provide defaults 
         [XmlIgnore]
-        public bool Stackable
+        public bool Stackable => Properties.Stackable != null;
+
+        [XmlIgnore]
+        public int MaximumStack => Properties.Stackable?.Max ?? 0;
+
+        [XmlIgnore]
+        public byte Level => Properties.Restrictions?.Level.Min ?? 1;
+
+        [XmlIgnore]
+        public byte Ability => Properties.Restrictions?.Ab.Min ?? 0;
+
+        [XmlIgnore]
+        public Element Element
         {
-            get { return Properties.Stackable != null; }
+            get
+            {
+                if (Properties.Equipment.WeaponType == WeaponType.None)
+                    return Properties.StatEffects?.Element?.Defense ?? Element.None;
+                return Properties.StatEffects?.Element?.Offense ?? Element.None;
+            }
         }
 
         [XmlIgnore]
+        public bool Usable => Properties.Use != null;
+
+        [XmlIgnore]
+        public Use Use => Properties.Use;
+
+        [XmlIgnore]
+        public int BonusHP => Properties.StatEffects.Base?.Hp ?? 0;
+        [XmlIgnore]
+        public int BonusMP => Properties.StatEffects.Base?.Mp ?? 0;
+
+        [XmlIgnore]
+        public Class Class => Properties.Restrictions?.@Class ?? Class.Peasant;
+        [XmlIgnore]
+        public Gender Gender => Properties.Restrictions?.Gender ?? Gender.Neutral;
+
+        [XmlIgnore]
+        public int BonusHp => Properties.StatEffects?.@Base?.Hp ?? 0;
+        [XmlIgnore]
+        public int BonusMp => Properties.StatEffects?.@Base?.Mp ?? 0;
+        [XmlIgnore]
+        public sbyte BonusStr => Properties.StatEffects?.@Base?.Str ?? 0;
+        [XmlIgnore]
+        public sbyte BonusInt => Properties.StatEffects?.@Base.@Int ?? 0;
+        [XmlIgnore]
+        public sbyte BonusWis => Properties.StatEffects?.@Base?.Wis ?? 0;
+        [XmlIgnore]
+        public sbyte BonusCon => Properties.StatEffects?.@Base?.Con ?? 0;
+        [XmlIgnore]
+        public sbyte BonusDex => Properties.StatEffects?.@Base?.Dex ?? 0;
+        [XmlIgnore]
+        public sbyte BonusDmg => Properties.StatEffects?.Combat?.Dmg ?? 0;
+        [XmlIgnore]
+        public sbyte BonusHit => Properties.StatEffects?.Combat?.Hit ?? 0;
+        [XmlIgnore]
+        public sbyte BonusAc => Properties.StatEffects?.Combat?.Ac ?? 0;
+        [XmlIgnore]
+        public sbyte BonusMr => Properties.StatEffects?.Combat?.Mr ?? 0;
+        [XmlIgnore]
+        public sbyte BonusRegen => Properties.StatEffects?.Combat?.Regen ?? 0;
+
+        [XmlIgnore]
+        public ushort MinLDamage => Properties.Damage?.Large.Min ?? 0;
+        [XmlIgnore]
+        public ushort MaxLDamage => Properties.Damage?.Large.Max ?? 0;
+        [XmlIgnore]
+        public ushort MinSDamage => Properties.Damage?.Small.Min ?? 0;
+        [XmlIgnore]
+        public ushort MaxSDamage => Properties.Damage?.Small.Max ?? 0;
+
+        [XmlIgnore]
         public Variant CurrentVariant { get; set; }
+
+        [XmlIgnore]
+        public sbyte Regen => Properties.StatEffects?.Combat?.Regen ?? 0;
+        #endregion
 
         [XmlIgnore]
         public Dictionary<int, Item> Variants { get; set; }
